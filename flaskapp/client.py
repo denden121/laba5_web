571143f8016bf0fabeb3f82c8c3bd2aa296454cd
denden121 <50229720+denden121@users.noreply.github.com>
2020-05-18 07:53:52 -0500

    Update client.py

--- flaskapp/client.py
+++ flaskapp/client.py
@@ -5,8 +5,8 @@ import base64
 img_data = None
 # создаем путь к файлу (для кроссплатформенности, например)
 path = os.path.join('./static','image0008.png')
-11
+
 # читаем файл и енкодируем его в строку base64
 with open(path, 'rb') as fh:
-img_data = fh.read()
-b64 = base64.b64encode(img_data)
+    img_data = fh.read()
+    b64 = base64.b64encode(img_data)
